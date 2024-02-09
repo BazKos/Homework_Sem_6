c5b52744a289e5de2b057d8c599111d1c649f4c8
Konstantin <konstbaz@gmail.com>
2024-02-10 02:11:18 +0300

    Решил 2 задачу

---
+++ Task6_2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задайте строку, содержащую латинские буквы в обоих регистрах. 
+//Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        string lowLit(string word)
+        {
+            return word.ToLower();
+        }
+
+        Console.Write("Введите любой текст латинскими буквами: ");
+        string text = Console.ReadLine()!;
+
+        string result = lowLit(text);
+        Console.WriteLine(result);
+    }
+}
