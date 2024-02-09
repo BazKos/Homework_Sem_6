a0d22d6890610bb9ce9c9686a01b26ffae9048ad
Konstantin <konstbaz@gmail.com>
2024-02-10 01:33:25 +0300

    Решил 1 задачу

--- Task6_1/Program.cs
+++ Task6_1/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿//Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива. CreateStringFrom2DArray
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        char[,] phrase = { {'H', 'e', 'l', 'l', 'o', ','}, {'w', 'o', 'r', 'l', 'd', '!'} };
+        foreach(char i in phrase)
+        {
+            Console.Write($"{i} ");
+        }
+
+        string StringFromPhrase (char[,] phrase)
+        {
+            string result = "";
+            for(int i = 0; i < phrase.GetLength(0); i++)
+            {
+                for(int j = 0; j < phrase.GetLength(1); j++)
+                {
+                    result += phrase[i, j];
+                }
+            }
+            return result;
+        }
+
+        string result = StringFromPhrase(phrase);
+        Console.WriteLine(result);
+
+    }
+}
