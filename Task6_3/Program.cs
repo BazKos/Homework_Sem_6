e492a3f62ccc754475d49901e3dc987a6bf06c91
Konstantin <konstbaz@gmail.com>
2024-02-10 03:54:32 +0300

    Решил 3 задачу

---
+++ Task6_3/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите слово для проверки на палидром: ");
+        string word = Console.ReadLine()!;
+
+        bool Palidrome(string word)
+        {
+            int size = word.Length;
+            for(int i = 0; i < size / 2; i++)
+            {
+                if(word[i] != word[size - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        bool result = Palidrome(word);
+        Console.WriteLine($"Результат проверки - {result} ");     
+    }
+}
